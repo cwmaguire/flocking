c035d3f4229c41f8b78515d37e45cd7fb1ad0f8d
Chris Maguire <cwmaguire@gmail.com>
2013-09-04 22:42:53 -0700

    Moved distance to geometry.js

--- movement/flock.js
+++ movement/flock.js
@@ -33,14 +33,6 @@ function findNeighboursInRange(point, boids, range, boidDistances){
   return findNeighboursInRange(point, boids.slice(1), range, newBoidDistances);
 }
 
-function distance(p1, p2){
-  var a = Math.abs(p1.x - p2.x);
-  var b = Math.abs(p1.y - p2.y);
-  var aSquared = Math.pow(a, 2);
-  var bSquared = Math.pow(b, 2);
-  return Math.sqrt(aSquared + bSquared);
-}
-
 function adjustToNeighbours(point, boidDistances, range, velocity, adjustments){
   if(adjustments === undefined){
     adjustments = [];
